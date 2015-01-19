951ee25f71166061ff50b93b491d4deafa055312
Dan <dcardin2007@gmail.com>
2015-01-19 11:16:25 -0500

    change case filters to use mb functions

--- src/TextAnalysis/Filters/UpperCaseFilter.php
+++ src/TextAnalysis/Filters/UpperCaseFilter.php
@@ -4,20 +4,20 @@ namespace TextAnalysis\Filters;
 use TextAnalysis\Interfaces\ITokenTransformation;
 
 /**
- * Normalize the text to lower case
+ * Normalize the text to upper case
  * @author Dan Cardin
  */
-class LowerCaseFilter implements ITokenTransformation
+class UpperCaseFilter implements ITokenTransformation
 {
     
     /**
-     * Lower case the word and return it
+     * Upper case the word and return it
      * @param string $word
      * @return string 
      */
     public function transform($word)
     {
-        return mb_strtolower($word);
+        return mb_strtoupper($word);
     }
 }
 
