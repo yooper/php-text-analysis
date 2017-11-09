e4fe1867ff433777ab9959a478127a8d1cd57177
Dan Cardin <dcardin2007@gmail.com>
2017-11-08 21:05:32 -0500

    fix unit test bug

--- tests/TextAnalysis/Tokenizers/TwitterTokenizerTest.php
+++ tests/TextAnalysis/Tokenizers/TwitterTokenizerTest.php
@@ -20,6 +20,6 @@ class TwitterTokenizerTest extends \PHPUnit_Framework_TestCase
     public function testForUrlAndEmail()
     {
         $tokens = (new TwitterTokenizer)->tokenize('Custom Software Development http://redbeardtechnologies.com/ 906-555-5555 or contact support at support@redbeardtechnologies.com :-)');
-        $this->assertCount(10, $tokens);        
+        $this->assertCount(11, $tokens);        
     }    
 }
