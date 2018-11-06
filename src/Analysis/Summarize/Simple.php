ad08210ba6999eeea458770346264ebc43ad88c6
Dan Cardin <dcardin2007@gmail.com>
2018-11-06 03:39:24 -0500

    master check in

---
+++ src/Analysis/Summarize/Simple.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TextAnalysis\Analysis\Summarize;
+
+use TextAnalysis\Models\ScoreKeeper;
+
+/**
+ * A simple algorithm based off of frequency counts for finding the best
+ * sentence to summarize the text
+ * @author yooper
+ */
+class Simple 
+{    
+    /**
+     * Returns each sentenced scored. 
+     * @param array $wordTokens
+     * @param array $sentenceTokens
+     * @return array
+     */
+    public function summarize(array $wordTokens, array $sentenceTokens) : array
+    {
+        $tokenCounts = array_count_values($wordTokens);
+        $scoreKeepers = [];
+        for($index = 0; $index < count($sentenceTokens); $index++)
+        {
+            $scoreKeepers[] = new ScoreKeeper($sentenceTokens[$index], $index);
+        }
+                       
+        foreach($tokenCounts as $token => $freq)
+        {
+            foreach($scoreKeepers as $sentenceKeeper)
+            {
+                if(strpos($sentenceKeeper->getToken(), $token) !== false) {
+                    
+                    $sentenceKeeper->addToScore($freq);
+                }
+            }
+        } 
+        
+        usort($scoreKeepers, 'score_keeper_sort');
+        return $scoreKeepers;
+    }
+        
+}
---
+++ tests/TextAnalysis/Analysis/Summarize/SimpleTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\TextAnalysis\Analysis\Summarize;
+
+/**
+ * Test out the simple summary algorithm
+ * @author yooper
+ */
+class SimpleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSimpleWithStopwords()
+    {
+        $stopwords = get_stop_words(VENDOR_DIR."yooper/stop-words/data/stop-words_english_1_en.txt"); 
+        $stopwords = array_map(function($word){ return " {$word} ";}, $stopwords);
+        $bestSentences = summary_simple($this->getArticle(), $stopwords);     
+        $this->assertCount(13, $bestSentences);
+        $this->assertEquals($this->getTopSentence(), $bestSentences[0]);
+                      
+    }
+    
+    public function testSimpleWithoutStopwords()
+    {
+        $bestSentences = summary_simple($this->getArticle());     
+        $this->assertCount(13, $bestSentences);
+        $this->assertNotEquals($this->getTopSentenceWithoutStopwords(), $bestSentences[0]);              
+    }    
+    
+    public function getArticle() : string
+    {
+        return <<<TEXT
+According to a Tuesday news release, Houghton County leaders are asking for a slowing of supply donations.
+
+Volunteers and financial donations are still needed, along with dehumidifiers, box fans or large equipment that can be used for excavating, demolition or reconstruction.
+
+"The response to our recent flood disaster has been overwhelming, and the Copper Country cannot be thankful enough for the support that’s been received," said Michael Babcock, the director of marketing and communications at Finlandia University. "However, as of now, volunteers have reached a point where enough normal supplies are on hand. Officials are now asking that the flow of general donations be reduced or stopped with a few exceptions. We know of several additional semi loads coming, but we’re now asking that any additional large deliveries that are planned please be put on hold, unless the items being donated are dehumidifiers, box fans or large equipment that can be used for excavating, demolition or reconstruction."
+
+Volunteers are still needed. The recovery effort is transitioning from initial clean-up to the rehab and reconstruction phase, and additional volunteers are a vital part of that effort.
+
+To donate money, please go to coppercountrystrong.com/donate.
+
+On Friday at 4 p.m. the Flood Relief Supply Distribution at Dee Stadium will be closing. Those in need of supplies are asked to stop by before it closes to get what’s needed for the weekend. Next steps for the distribution center are being evaluated and will be announced as soon as possible.        
+TEXT;
+    }
+    
+    public function getTopSentence()
+    {
+        return '"the response to our recent flood disaster has been overwhelming, and the copper country cannot be thankful enough for the support that\'s been received," said michael babcock, the director of marketing and communications at finlandia university.';
+    }
+    
+    public function getTopSentenceWithoutStopwords()
+    {
+        return 'we know of several additional semi loads coming, but we’re now asking that any additional large deliveries that are planned please be put on hold, unless the items being donated are dehumidifiers, box fans or large equipment that can be used for excavating, demolition or reconstruction."';
+    }
+}
