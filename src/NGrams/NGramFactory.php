fbb7ec55c0302c6ce8a4914f04eed1ecc9f74297
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-02-05 17:40:49 -0200

    Added ngram frequency
    Added ngram frequency and placed the ngram string as the key of the array.

--- src/NGrams/NGramFactory.php
+++ src/NGrams/NGramFactory.php
@@ -13,40 +13,47 @@ class NGramFactory
     const TRIGRAM = 3;
 
     /**
-     * Protect the constructor
-     */
+    * Protect the constructor
+    */
     protected function __construct(){}
 
-    /**
-     * Generate Ngrams from the tokens
-     * @param array $tokens
-     * @param int $nGramSize
-     * @return  array return an array of the ngrams
-     */
-    static public function create(array $tokens, $nGramSize = self::BIGRAM, $separator = ' ') : array
-    {
-        $freq = freq_dist($tokens)->getKeyValuesByFrequency();
-        $separatorLength = strlen($separator);
-        $length = count($tokens) - $nGramSize + 1;
-        if($length < 1) {
-            return [];
-        }
-        $ngrams = array_fill(0, $length, ''); // initialize the array
-
-        for($index = 0; $index < $length; $index++)
+        /**
+        * Generate Ngrams from the tokens
+        * @param array $tokens
+        * @param int $nGramSize
+        * @return  array return an array of the ngrams
+        */
+        static public function create(array $tokens, $nGramSize = self::BIGRAM, $separator = ' ') : array
         {
-            $ngram = array(0 => '');
-            for($jindex = 0; $jindex < $nGramSize; $jindex++)
+            $freq = freq_dist($tokens)->getKeyValuesByFrequency();
+            $separatorLength = strlen($separator);
+            $length = count($tokens) - $nGramSize + 1;
+            if($length < 1) {
+                return [];
+            }
+            $ngrams = array(); // initialize the array
+
+            for($index = 0; $index < $length; $index++)
             {
-                $ngram[0] .= $tokens[$index + $jindex];
-                $ngram[]   = $freq[$tokens[$index + $jindex]];
-                $ngrams[$index] = $ngram;
-                if($jindex < $nGramSize - $separatorLength) {
-                    $ngram[0] .= $separator;
-                    $ngrams[$index] = $ngram;
+                $ngram_string = '';
+                $ngram = array();
+                for($jindex = 0; $jindex < $nGramSize; $jindex++)
+                {
+                    $ngram_string .= $tokens[$index + $jindex];
+                    $ngram[]   = $freq[$tokens[$index + $jindex]]; // adds the token frequency
+                    if($jindex < $nGramSize - $separatorLength) {
+                        $ngram_string .= $separator;
+                    } else {
+                        if(isset($ngrams[$ngram_string][$nGramSize]) || array_key_exists($ngram_string, $ngrams)) { // checks if it was already counted
+                            $ngrams[$ngram_string][$nGramSize]++;
+                        }else {
+                            $ngram[$nGramSize] = 1;
+                            $ngrams[$ngram_string] = $ngram;
+                        }
+                    }
                 }
             }
+
+            return $ngrams;
         }
-        return $ngrams;
-    }
 }
